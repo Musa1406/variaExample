6f0e95dec3c7a2d36d5115a077ed61bea28b0b86
Musa1406 <91537282+Musa1406@users.noreply.github.com>
2021-09-28 16:45:35 +0300

    Initial commit

---
+++ src/variaExample/Main.java
@@ -0,0 +1,33 @@
+package variaExample;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        String phoneName="SM-G998B";
+        String phoneRam="Ram :12GB";
+        String phoneLeght="Ekran Boyutu :6.8 Ýnch";
+        String phoneSell="Fiyat :14.308";
+        String phoneMah="Batarya :5000Mah";
+        String phoneDah="Dahili D. :256GB";
+        String phoneCPU="Ýþlemci :Samsung Exynos 2100";
+        String phoneGPU="Ekran Kartý :Mali-G78 MP14";
+        System.out.println(phoneName);
+        phoneName="SM-G9988";
+        System.out.println(phoneRam);
+        phoneRam="Ram :12GB";
+        System.out.println(phoneLeght);
+        phoneLeght="Ekran Boyutu:6.8 Ýnch";
+        System.out.println(phoneSell);
+        phoneSell="Fiyat :14.308";
+        System.out.println(phoneMah);
+        phoneMah="Batarya :5000MAh";
+        System.out.println(phoneDah);
+        phoneDah="Dahili D. :256Gb";
+        System.out.println(phoneCPU);
+        phoneCPU="Ýþlemci :Samsung Exynos 2100";
+        System.out.println(phoneGPU);
+        phoneGPU="Ekran Kartý :Mali-G78 MP14";
+	}
+
+}
